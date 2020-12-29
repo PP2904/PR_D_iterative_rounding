0b9ed6dfcb750e7b0209adb5f97595dee3e1c6d3
Peter Pfrommer <>
2020-12-30 00:23:17 +0100

    for_schleife_

--- main.cpp
+++ main.cpp
@@ -20,8 +20,27 @@
 
 //floor function eingefügt
 
+//TODO:
+// der Bidder mit der größten Val eines Guts, bekommt es zugewiesen, sofern er es sich leisten kann!
+// (beginnend beim 1. Bidder)
 
-// gibt es pro Gut genau eine Einheit??? -> Annahme ja! (22.10.20)
+/*
+BSP:
+
+Höchste Valuation pro Gut (Achtung: Bidder werden ab 0 gezählt)
+(21,1) | (23,3) | (13,3) | (25,1) | (18,3) | (19,3) |
+
+
+Optimale Allokation:
+0 0 0 0 2.47 3  | 3 0 0 1.25 0 0  | 0 0 3 1.75 0 0  | 0 3 0 0 0.533 0  |
+
+Update integrale (gerundete) Allokation:
+0 0 0 0 2 3  | 3 0 0 1 0 0  | 0 0 3 1 0 0  | 0 3 0 0 0 0  |
+
+*/
+
+
+
 
 using namespace std;
 
@@ -109,20 +128,12 @@ int main() {
     ofstream myfile2;
     myfile2.open("result.txt", std::ios_base::app);
 
-    myfile2 << "Number Goods: " << num_goods << ", " << " Number Bidders: " << num_bidders << ", "  << " Number Iterations: " << num_iterations << "\n";
+    myfile2 << "Number Goods: " << num_goods << ", " << " Number Bidders: " << num_bidders << ", "
+            << " Number Iterations: " << num_iterations << ", " << " Quantity per item: " << quantItem << "\n";
     myfile2 << "integral util | fractional util" << "\n";
 
     //FOR SCHLEIFE FÜR ANZAHL WIEDERHOLUNGEN DES GESAMTEXPERIMENTS
     for (int iter = 1; iter < num_iter_exp; iter++) {
-
-
-        /*
-        *
-        * BIS HIER GEBEN WIR DIE EIGENSCHAFTEN DES EXPERIMENTS AN
-        *
-        *
-        *
-        */
 
 
         vector<Bidder> bidders(num_bidders);
@@ -172,21 +183,15 @@ int main() {
             }
             cout << endl;
 
-            //writing to txt file
-
-            /*ofstream myfile;
-            myfile.open ("markets.txt", std::ios_base::app);
-            myfile << "Iteration " << it << ":\n";
-            for (int i = 0; i < bidders.size(); ++i) {
-                myfile << "Bidder " << i << ": " << bidders[i] << endl;
-            }
-            myfile << endl;*/
         }
 
         //von Max utility und utility (im equilibrium sind diese gleich)
 
         vector<double> utility(num_bidders);
         vector<double> max_utility(num_bidders);
+
+        vector<double> rd_max_utility(num_bidders);
+
         for (int b = 0; b < num_bidders; ++b) {
             max_utility[b] = 0;
             for (int i = 0; i < num_goods; ++i) {
@@ -236,36 +241,6 @@ int main() {
             //max_util = max_util + max_utility[i];
         }
 
-        //cout << "Summe Max Utility: " << max_util;
-
-
-
-        //                      //
-        //                      //
-        //                      //
-        // writing to txt file  //
-        //                      //
-        //                      //
-        //                      //
-
-        /*  *//*ofstream myfile;
-    myfile.open ("markets.txt", std::ios_base::app);*//*
-    myfile << endl;
-    myfile << "D/MaxUtil: " << endl;
-    for (int j = 0; j < num_goods; ++j) {
-        double demand = 0;
-        double supply = 0;
-        for (int i = 0; i < bidders.size(); ++i) {
-            demand += bidders[i].spent[j] / prices[j];
-            myfile << "Bidder " << i << " receives " << bidders[i].spent[j] / prices[j] << "of good " << j << endl;
-        }
-        myfile << "Demand: " << demand << endl;
-        myfile << "\n";
-    }
-     myfile.close();
-
-
-*/
 
 
 /* Kantengewichte sind die Allokationen der Güter auf die Bieter!! nicht die valuations */
@@ -291,171 +266,158 @@ int main() {
                 frac = frac + (quantItem * (graph[i][j]) - floor(quantItem * (graph[i][j])));
             }
 
-            /*fracVec[j] = round(frac);
-            cout << std::setprecision(pre) << frac << " ";
-            cout << " | ";
-            if (j == (num_goods - 1)) {
-                myfile2 << std::setprecision(pre) << frac;
-                continue;
+
+
+
+            //sortiere höchste valuation für jeweiliges gut raus
+            cout << "\n";
+            cout << "Höchste Valuation pro Gut (Achtung: Bidder werden ab 0 gezählt) \n ";
+
+            vector<pair<int, int> > vecPair(num_goods);
+
+            //höchste Valuation für jeweils ein Gut = Entscheidung wem die fraktionalen Teilen eines Guts
+            // zugewwiesen werden
+
+            int greatest_val = 0;
+            for (int i = 0; i < num_goods; ++i) {
+                for (int b = 0; b < num_bidders; ++b) {
+                    if (bidders[b].valuation[i] >= greatest_val) {
+                        greatest_val = bidders[b].valuation[i];
+                        vecPair[i] = make_pair(greatest_val, b);
+                    }
+
+                }
+                cout << "(" << vecPair[i].first << "," << vecPair[i].second << ")";
+                cout << " | ";
+                greatest_val = 0;
             }
-            myfile2 << std::setprecision(pre) << frac << " , ";
-            frac = 0;*/
-        }
-        /* myfile2 << endl;
-         myfile2.close();*/
+            cout << endl;
+
+            vector<vector<int>> up_integral(num_bidders, vector<int>(num_goods));
+            //Initialisiere mit integraler Allokation
+            /*up_integral := die integralen Ergebnisse, nachdem die fraktionalen Teile den jeweiligen Biddern
+            zugeteilt wurden */
+            for (int i = 0; i < num_bidders; ++i) {
+                for (int j = 0; j < num_goods; ++j) {
+                    up_integral[i][j] = floor(quantItem * (graph[i][j]));
+                }
+            }
+            for (int j = 0; j < num_goods; ++j) {
+                up_integral[vecPair[j].second][j] += fracVec[j];
+            }
+
+
+            cout << "\n";
+            cout << "\n";
+            cout << "Optimale Allokation: \n";
+            /*** print graph ***/
+            for (int i = 0; i < num_bidders; ++i) {
+                for (int j = 0; j < num_goods; ++j) {
+                    if ((quantItem * (graph[i][j])) < 0.001) {
+                        graph[i][j] = 0;
+                    }
+                    cout << quantItem * (graph[i][j]) << " ";
+                }
+                cout << " | ";
+            }
+            cout << "\n";
+
+            //neue Allokation nach dem Runden der Allokationen:
+            cout << "\n";
+            cout << "Update integrale (gerundete) Allokation: \n";
+            for (int i = 0; i < num_bidders; ++i) {
+                for (int j = 0; j < num_goods; ++j) {
+                    cout << up_integral[i][j] << " ";
+                }
+                cout << " | ";
+            }
+            cout << endl;
 
 
 
-        //sortiere höchste valuation für jeweiliges gut raus
-        cout << "\n";
-        cout << "Höchste Valuation pro Gut (Achtung: Bidder werden ab 0 gezählt) \n ";
+            //max_utility der gerundeten Alloks berechnen
+            cout << "\n";
+            cout << "\n";
+            cout << "\n";
+            cout << "integrality gap: \n";
+            double rd_util = 0.0;
 
-        vector<pair<int, int> > vecPair(num_goods);
 
-        //höchste Valuation für jeweils ein Gut = Entscheidung wem die fraktionalen Teilen eines Guts
-        // zugewwiesen werden
+            double int_gap = 0.0;
+            double print_int_gap = 0.0;
+            double avg_int_gap = 0.0;
 
-        int greatest_val = 0;
-        for (int i = 0; i < num_goods; ++i) {
-            for (int b = 0; b < num_bidders; ++b) {
-                if (bidders[b].valuation[i] >= greatest_val) {
-                    greatest_val = bidders[b].valuation[i];
-                    vecPair[i] = make_pair(greatest_val, b);
+
+            for (int i = 0; i < num_bidders; ++i) {
+                for (int j = 0; j < num_goods; ++j) {
+                    rd_util = rd_util + (((up_integral[i][j]) / quantItem) * bidders[i].valuation[j]);
+                }
+                rd_max_utility[i] = rd_util;
+
+                /* if(rd_max_utility[i] <= max_utility[i]) {
+                     myfile2 << rd_max_utility[i] << " | ";
+                     myfile2 << std::setprecision(pre) << max_utility[i] << "\n";
+                 }*/
+
+
+                if (rd_max_utility[i] <= max_utility[i]) {
+                    cout << std::setprecision(pre) << rd_max_utility[i] / max_utility[i] << "\n";
+                    //myfile << std::setprecision(pre)  << rd_max_utility[i]/max_utility[i] << "\n";
+                    int_gap = int_gap + (rd_max_utility[i] / max_utility[i]);
                 }
 
-            }
-            cout << "(" << vecPair[i].first << "," << vecPair[i].second << ")";
-            cout << " | ";
-            greatest_val = 0;
-        }
-        cout << endl;
 
-        vector<vector<int>> up_integral(num_bidders, vector<int>(num_goods));
-        //Initialisiere mit integraler Allokation
-        /*up_integral := die integralen Ergebnisse, nachdem die fraktionalen Teile den jeweiligen Biddern
-        zugeteilt wurden */
-        for (int i = 0; i < num_bidders; ++i) {
-            for (int j = 0; j < num_goods; ++j) {
-                up_integral[i][j] = floor(quantItem * (graph[i][j]));
-            }
-        }
-        for (int j = 0; j < num_goods; ++j) {
-            up_integral[vecPair[j].second][j] += fracVec[j];
-        }
+                /*if(rd_max_utility[i] > max_utility[i]){
+                    cout << std::setprecision(pre)  << max_utility[i]/rd_max_utility[i] << "\n";
+                    //myfile << std::setprecision(pre)  << max_utility[i]/rd_max_utility[i] << "\n";
+                    int_gap = int_gap + (max_utility[i]/rd_max_utility[i]);
+                }*/
 
 
-        cout << "\n";
-        cout << "\n";
-        cout << "Optimale Allokation: \n";
-        /*** print graph ***/
-        for (int i = 0; i < num_bidders; ++i) {
-            for (int j = 0; j < num_goods; ++j) {
-                if ((quantItem * (graph[i][j])) < 0.001) {
-                    graph[i][j] = 0;
+                print_int_gap = int_gap;
+                if (i == (num_bidders - 1)) {
+                    avg_int_gap = print_int_gap; // /num_bidders;
                 }
-                cout << quantItem * (graph[i][j]) << " ";
-            }
-            cout << " | ";
-        }
-        cout << "\n";
-
-        //neue Allokation nach dem Runden der Allokationen:
-        cout << "\n";
-        cout << "Update integrale (gerundete) Allokation: \n";
-        for (int i = 0; i < num_bidders; ++i) {
-            for (int j = 0; j < num_goods; ++j) {
-                cout << up_integral[i][j] << " ";
-            }
-            cout << " | ";
-        }
-        cout << endl;
-
-
-
-        //max_utility der gerundeten Alloks berechnen
-        cout << "\n";
-        cout << "\n";
-        cout << "\n";
-        cout << "integrality gap: \n";
-        double rd_util = 0.0;
-        vector<double> rd_max_utility(num_bidders);
-
-        double int_gap = 0.0;
-        double print_int_gap = 0.0;
-        double avg_int_gap = 0.0;
-
-
-
-        for (int i = 0; i < num_bidders; ++i) {
-            for (int j = 0; j < num_goods; ++j) {
-                rd_util = rd_util + (((up_integral[i][j]) / quantItem) * bidders[i].valuation[j]);
-            }
-            rd_max_utility[i] = rd_util;
-
-            if(rd_max_utility[i] <= max_utility[i]) {
-                myfile2 << rd_max_utility[i] << " | ";
-                myfile2 << std::setprecision(pre) << max_utility[i] << "\n";
+                int_gap = 0.0;
+                rd_util = 0.0;
             }
 
 
-            if(rd_max_utility[i] <= max_utility[i]){
-                cout << std::setprecision(pre)  << rd_max_utility[i]/max_utility[i] << "\n";
-                //myfile << std::setprecision(pre)  << rd_max_utility[i]/max_utility[i] << "\n";
-                int_gap = int_gap + (rd_max_utility[i]/max_utility[i]);
+            auto end = std::chrono::system_clock::now();
+
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+            if (iter == (num_iter_exp - 1)) {
+                cout << "finished computation at " << std::ctime(&end_time)
+                     << "elapsed time: " << elapsed_seconds.count() << "s\n";
             }
 
 
-            /*if(rd_max_utility[i] > max_utility[i]){
-                cout << std::setprecision(pre)  << max_utility[i]/rd_max_utility[i] << "\n";
-                //myfile << std::setprecision(pre)  << max_utility[i]/rd_max_utility[i] << "\n";
-                int_gap = int_gap + (max_utility[i]/rd_max_utility[i]);
-            }*/
-
-            print_int_gap = int_gap;
-            if(i==(num_bidders-1)){
-                avg_int_gap = print_int_gap; // /num_bidders;
-            }
-            int_gap = 0.0;
-            rd_util = 0.0;
         }
 
-        /*myfile2 << "Integrality gap is in average: " << avg_int_gap;
-        myfile2 << "\n";
-        myfile2 << "\n";
-        myfile2 << "\n";*/
 
+        if (iter == ( num_iter_exp - 1)) {
+            for (int i = 0; i < num_bidders; ++i) {
+                if (rd_max_utility[i] <= max_utility[i]) {
+                    myfile2 << rd_max_utility[i] << " | " << max_utility[i] << "\n";
+                }
+            }
 
+            myfile2 << "\n";
 
-
-        //vergleich optimal und gerundet
-
-        //max_utility[i] = optimal
-        //rd_max_utility[i] = gerundet
-
-        /*ofstream myfile;
-        myfile.open("markets.txt", std::ios_base::app);
+           /* //unnötig, weil opt immer besser als rounded solution
+            for (int i = 0; i < num_bidders; ++i) {
+                myfile2 << rd_max_utility[i] << " | " << max_utility[i] << "\n";
+            }
 */
+        }
 
-
-
-
-
-
-
-    auto end = std::chrono::system_clock::now();
-
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-    if(iter == (num_iter_exp-1)){
-        cout << "finished computation at " << std::ctime(&end_time)
-                << "elapsed time: " << elapsed_seconds.count() << "s\n";
-    }
 
     }
 
     return 0;
 
+
+
 }
-
-
